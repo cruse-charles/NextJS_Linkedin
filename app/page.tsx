8db74a22a7c617ff8679373237404633ae60f47f
Charles Cruse <charle9@vt.edu>
2025-01-09 10:14:18 +0800

    Added columns for layout of main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import UserInformation from "@/components/UserInformation";
 
 export default function Home() {
   return (
-    <div className="grid">
-      <section>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline col-span-2">
         {/* User Info */}
         <UserInformation />
       </section>
-      <section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4  xl:max-w-lg mx-auto w-full">
         {/* Post Form */}
         {/* Post Feed */}
       </section>
-      <section>
+      <section className="hidden xl:inline justify-center col-span-2">
         {/* Widget */}
       </section>
     </div>
